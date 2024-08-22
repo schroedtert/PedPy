087c7f4b8b9093e3be545f9083ddd258b2654828
schroedtert <t.schroedter@posteo.de>
2024-08-22 20:22:12 +0200

    Fix crossing frames when persons stand on line
    When persons stand on the line, they were taken into account multiple
    times when computing the crossing frames. Per definition a movement
    counts as crossing the line, when the endpoint is not on line. This
    condition was missing here.

--- pedpy/methods/method_utils.py
+++ pedpy/methods/method_utils.py
@@ -567,9 +567,10 @@ def compute_crossing_frames(
     )
 
     # crossing means, the current movement crosses the line and the end point
-    # of the movement is not on the line. The result is sorted by frame number
+    # of the movement is not on the line.
     crossing_frames = df_movement.loc[
-        shapely.intersects(df_movement.movement, measurement_line.line)
+        (shapely.intersects(df_movement.movement, measurement_line.line))
+        & (~shapely.intersects(df_movement.end_position, measurement_line.line))
     ][[ID_COL, FRAME_COL]]
 
     return crossing_frames
